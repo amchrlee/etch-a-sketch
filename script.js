58a7b9f57fd9fa9096f6fe2700f33332168830aa
Amy Lee <39871117+amchrlee@users.noreply.github.com>
2023-02-27 14:34:03 -0500

    Set up hover effect on grid divs for color change

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-function createGrid (rows, cols) {
+function createGrid(rows, cols) {
     const gridContainer = document.querySelector(".grid-container");
     gridContainer.style.setProperty("--grid-rows", rows);
     gridContainer.style.setProperty("--grid-cols", cols);
 
-    for (let i = 0; i < (rows * cols); i++) {
-            let cell = document.createElement("div");
-            cell.className = "cell";
-            gridContainer.appendChild(cell);
+    for (let i = 1; i <= (rows * cols); i++) {
+        let cell = document.createElement("div");
+        cell.className = "cell";
+        cell.setAttribute("value", i);
+        gridContainer.appendChild(cell);
     };
 };
 
+function colorCell(e) {
+    e.currentTarget.style.cssText = `
+    background-color: black;`;
+}
+
 createGrid(16, 16);
+
+let cellDiv = document.querySelector(".cell");
+
+let cellDivAll = document.querySelectorAll(".cell");
+
+cellDivAll.forEach((cellDiv) => {
+    cellDiv.addEventListener("mouseover", colorCell);
+});
